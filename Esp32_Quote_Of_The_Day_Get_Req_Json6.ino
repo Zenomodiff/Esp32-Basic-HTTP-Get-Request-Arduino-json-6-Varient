d6a313247d78c6dafe8fdcb36e12bf40d5c2efda
Sreeram Aj <sreeramzeno@gmail.com>
2021-06-09 00:17:01 +0530

    Add files via upload

---
+++ Esp32_Quote_Of_The_Day_Get_Req_Json6.ino
@@ -0,0 +1,55 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+
+const char* ssid = "Zeno";
+const char* password = "9061116368";
+
+void setup() {
+
+Serial.begin(115200);
+WiFi.begin(ssid, password);
+
+while (WiFi.status() != WL_CONNECTED) {
+delay(1000);
+Serial.print(".");
+}
+Serial.print("Connected");
+Serial.println();
+}
+
+void loop() {
+
+if ((WiFi.status() == WL_CONNECTED)) { 
+HTTPClient http;
+
+http.begin("https://zenquotes.io/api/random");
+
+int httpCode = http.GET();                                        
+if (httpCode > 0) { 
+String payload = http.getString();
+StaticJsonDocument<1024> doc;
+DeserializationError error = deserializeJson(doc, payload);
+
+JsonObject root_0 = doc[0];
+
+const char* root_0_q = root_0["q"];
+const char* root_0_a = root_0["a"];
+
+Serial.println();
+
+Serial.print("Author : "); Serial.println(root_0_a);
+Serial.print("Quote : "); Serial.println(root_0_q);
+
+    }
+
+    else {
+      Serial.println("Error on HTTP request");
+    }
+
+    http.end();
+    }
+
+  delay(10000);
+
+}
